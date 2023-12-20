d61309d4e4e625e34f5fcca91063c7b488c040c8
SWEET-DEV-8\phong <phongphanq089@gmail.com>
2023-12-20 09:04:15 +0700

    add commit three

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
+console.log(MONGODB_URI);
+
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
