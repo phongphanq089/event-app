3c21425c61a094188c2bd3d1734fb6d01fa0493e
SWEET-DEV-8\phong <phongphanq089@gmail.com>
2023-12-20 14:50:32 +0700

    update folder webhooks

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -9,6 +9,8 @@ export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
+  console.log(WEBHOOK_SECRET, "dsuf90gdfgdf0gufdfgdfnhjk");
+
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
@@ -55,6 +57,12 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+
+  console.log("Webhook body:", body);
+
+  console.log(eventType, "dsuf90gdfgdf0gufdfgdfnhjk");
+
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
@@ -67,6 +75,7 @@ export async function POST(req: Request) {
       lastName: last_name,
       photo: image_url,
     };
+
     const newUser = await createUser(user);
 
     if (newUser) {
